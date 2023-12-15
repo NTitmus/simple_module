579918227488a7b752857a5d690a158951a0af90
Nicola Titmus <nicola.koo@cantab.net>
2023-12-15 08:51:46 +0000

    corrected errors in module, still has ami error

--- modules/module_instance/main.tf
+++ modules/module_instance/main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
 resource "aws_instance" "example" {
-    instance_type = "t2.microzzz"
+    instance_type = "t2.micro"
     ami = var.instance_ami
 }
