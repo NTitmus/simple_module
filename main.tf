c3bad3d31efa4c0910942a6aaeb556a10bbdb6d4
Nicola Titmus <nicola.koo@cantab.net>
2024-02-03 15:52:17 +0000

    corrected security issues in s3

--- main.tf
+++ main.tf
@@ -52,6 +52,41 @@ resource "aws_s3_bucket" "sonarqube_test" {
   }
 
   versioning {
-    enabled = false
+    enabled = true
   }
+}
+
+resource "aws_s3_bucket_policy" "mycompliantpolicy" {
+  bucket = "nt-sonar-test-bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "mycompliantpolicy"
+    Statement = [
+      {
+        Sid       = "HTTPSOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.sonarqube_test.arn,
+          "${aws_s3_bucket.sonarqube_test.arn}/*",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
+  bucket = aws_s3_bucket.sonarqube_test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
